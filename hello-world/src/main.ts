ebf34b1f86199d2d2a4ff72fc7f83e37a5329476
arkarlin2003 <arkarlin486@gmail.com>
2025-04-19 09:55:53 +0630

    feat(app): lighting and resize

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // initial scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color("#f0f0f0");
+// scene.background = new THREE.Color("#f0f0f0");
 
 // add objects to scene
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -12,7 +12,29 @@ const material = new THREE.MeshLambertMaterial({
   emissive: new THREE.Color("#FF0000").convertSRGBToLinear(), // Red
 });
 const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
 scene.add(cube);
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({ color: 0xcccccc })
+);
+plane.rotation.x = -Math.PI / 2;
+plane.position.y = -1;
+plane.receiveShadow = true;
+scene.add(plane);
+
+// lighting
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 10, 7);
+directionalLight.castShadow = true;
+
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 500;
+
+scene.add(directionalLight);
 
 // camera
 const camera = new THREE.PerspectiveCamera(
@@ -25,6 +47,8 @@ camera.position.z = 5;
 
 // renderer
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 // renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
@@ -34,8 +58,15 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.autoRotate = true;
 controls.enableZoom = true;
-// animate
 
+// resize
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// animate
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
