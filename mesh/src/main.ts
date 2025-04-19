9c447b39e0763e25c1e32c044f05532c57605b76
arkarlin2003 <arkarlin486@gmail.com>
2025-04-19 10:27:17 +0630

    feat(app): mesh transforming position

---
+++ mesh/src/main.ts
@@ -0,0 +1,87 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// initial scene
+const scene = new THREE.Scene();
+// scene.background = new THREE.Color("#f0f0f0");
+
+// add objects to scene
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshLambertMaterial({
+  color: new THREE.Color("#00FF00").convertSRGBToLinear(), // Green
+  emissive: new THREE.Color("#FF0000").convertSRGBToLinear(), // Red
+});
+const cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
+cube.position.y = 1;
+cube.position.z = -1;
+scene.add(cube);
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({ color: 0xcccccc })
+);
+plane.rotation.x = -Math.PI / 2;
+plane.position.y = -1;
+plane.receiveShadow = true;
+scene.add(plane);
+
+// axes helpers
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// lighting
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 10, 7);
+directionalLight.castShadow = true;
+
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 500;
+
+scene.add(directionalLight);
+
+// camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(window.innerWidth, window.innerHeight);
+// antialiasing problem solution
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// control
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+// controls.autoRotate = true;
+controls.enableZoom = true;
+
+// resize
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// animate
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
