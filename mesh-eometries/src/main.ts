90ae833d925bccf271f6d619e6774761740e95b4
arkarlin2003 <arkarlin486@gmail.com>
2025-04-29 09:54:50 +0630

    feat(app): tweakpane

--- mesh-eometries/src/main.ts
+++ mesh-eometries/src/main.ts
@@ -1,17 +1,59 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Pane } from "tweakpane";
+
 // initial scene
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color("#f0f0f0");
 
 // add objects to scene
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
+const boxParameters = {
+  width: 1,
+  height: 1,
+  depth: 1,
+};
+const geometry = new THREE.BoxGeometry(
+  boxParameters.width,
+  boxParameters.height,
+  boxParameters.depth,
+  2,
+  2,
+  2
+);
+// const geometry = new THREE.CapsuleGeometry(8.6, 1, 4, 8);
+// const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
 
 // custom geometry
-const vertices = new Float32Array([0, 0, 0, 1, 0, 0, 0, 1, 0]);
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+// const vertices = new Float32Array([0, 0, 0, 1, 0, 0, 0, 1, 0]);
+// const geometry = new THREE.BufferGeometry();
+// geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+
+// setup tweakpane
+const pane = new Pane();
+const folder = pane.addFolder({
+  title: "Title",
+  expanded: true,
+});
+
+// Helper function to update geometry
+const updateCubeGeometry = () => {
+  const { width, height, depth } = boxParameters;
+  cube.geometry.dispose(); // dispose old geometry (important to avoid memory leaks)
+  cube.geometry = new THREE.BoxGeometry(width, height, depth);
+};
+
+// Add bindings
+["width", "height", "depth"].forEach((dimension) => {
+  folder
+    .addBinding(boxParameters, dimension as "width" | "height" | "depth", {
+      min: 0.1,
+      max: 10,
+      step: 0.1,
+      label: dimension.charAt(0).toUpperCase() + dimension.slice(1),
+    })
+    .on("change", updateCubeGeometry);
+});
 
 const material = new THREE.MeshLambertMaterial({
   color: new THREE.Color("#00FF00").convertSRGBToLinear(), // Green
@@ -20,7 +62,7 @@ const material = new THREE.MeshLambertMaterial({
 });
 
 const cube = new THREE.Mesh(geometry, material);
-cube.scale.setScalar(2);
+// cube.scale.setScalar(2);
 scene.add(cube);
 
 // camera
@@ -36,11 +78,9 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 // antialiasing problem solution
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
