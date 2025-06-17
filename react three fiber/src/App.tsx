b89df961319c25efc496862150fe7fd5cbade7f2
arkarlin2003 <arkarlin486@gmail.com>
2025-06-17 11:23:11 +0630

    feat(app): debug , axies grid box helpers , leva and stats

--- react three fiber/src/App.tsx
+++ react three fiber/src/App.tsx
@@ -1,23 +1,63 @@
 // App.tsx
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import {
+  Center,
+  Grid,
+  OrbitControls,
+  PerspectiveCamera,
+  Stats,
+} from "@react-three/drei";
+import { useControls } from "leva";
 
 export default function App() {
-  return (
-    <Canvas
-      camera={{ position: [2, 2, 3] }}
-      shadows
-      style={{ height: "100dvh", width: "100%" }}
-    >
-      <Suspense fallback={null}>
-        <mesh position={[0, 1, -2]} rotation={[1, 2, 5]}>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
-        <axesHelper args={[5]} />
+  const centerPostions = useControls("Center Position", {
+    centerX: { value: 0, min: -10, max: 10, step: 0.1 },
+    centerY: { value: 2, min: -10, max: 10, step: 0.1 },
+    centerZ: { value: 0, min: -10, max: 10, step: 0.1 },
+  });
 
+  const gridControls = useControls("Grid", {
+    sectionColor: "#800080",
+    cellColor: "#6f6f6f",
+    sectionSize: 3,
+    sectionThickness: 1,
+    cellSize: 1,
+    cellThickness: 0.6,
+    infiniteGrid: true,
+    fadeDistance: 50,
+    fadeStrength: 5,
+  });
+
+  return (
+    <Canvas shadows style={{ height: "100dvh", width: "100%" }}>
+      <PerspectiveCamera position={[1, 2, 5]} makeDefault aspect={1} />
+
+      <Suspense fallback={null}>
+        <Center
+          top
+          left
+          position={[
+            centerPostions.centerX,
+            centerPostions.centerY,
+            centerPostions.centerZ,
+          ]}
+        >
+          <mesh>
+            <boxGeometry />
+            <meshNormalMaterial />
+          </mesh>
+          <mesh position={[2, 0, 0]}>
+            <boxGeometry />
+            <meshNormalMaterial />
+          </mesh>
+        </Center>
+
+        <Grid {...gridControls} />
+
+        <axesHelper args={[5]} />
         <OrbitControls />
+        <Stats />
       </Suspense>
     </Canvas>
   );
